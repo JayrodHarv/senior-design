51a889ae7a11aae37a52893a212d7d470869057b
Cole90-bit <colekimball919@gmail.com>
2026-08-28 17:39:58 -0500

    single functional temp sensor code for ESP32 dev board using DallasTemperature and OneWire libraries.

--- Lab01_Temperature_Reader/embedded/src/main.cpp
+++ Lab01_Temperature_Reader/embedded/src/main.cpp
@@ -1,36 +1,60 @@
-#include <Arduino.h>
-
-// put function declarations here:
-// int myFunction(int, int);
+// #include <Arduino.h>
 
 // void setup() {
-//   // put your setup code here, to run once:
-//   int result = myFunction(2, 3);
+  
 // }
 
 // void loop() {
 //   // put your main code here, to run repeatedly:
 // }
 
-// // put function definitions here:
-// int myFunction(int x, int y) {
-//   return x + y;
-// }
+//   //what for startup
+//   //ESP32 dev board espressif32
+//   //main loop
+//   //read DS18B20 serial data
+//   //send data to http server
 
-#ifndef LED_BUILTIN
-#define LED_BUILTIN 2
-#endif
+//   //busy wait when switch is off
+
+
+  #include <Arduino.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+// DS18B20 data pin
+#define ONE_WIRE_BUS 4
+
+// Create OneWire communication object
+OneWire oneWire(ONE_WIRE_BUS);
+
+// Connect DallasTemperature to the OneWire bus
+DallasTemperature sensors(&oneWire);
 
 void setup()
 {
-  pinMode(LED_BUILTIN, OUTPUT);
+    // Start Serial Monitor
+    Serial.begin(115200);
+
+    // Start the temperature sensor
+    sensors.begin();
+
+    Serial.println("DS18B20 Temperature Sensor");
+    Serial.println("--------------------------");
 }
 
 void loop()
 {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);
-  
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(1000);
+    // Ask the DS18B20 to measure the temperature
+    sensors.requestTemperatures();
+
+    // Get temperature from the first sensor
+    float temperatureC = sensors.getTempCByIndex(0);
+
+    // Print temperature
+    Serial.print("Temperature: ");
+    Serial.print(temperatureC);
+    Serial.println(" °C");
+
+    // Wait 2 seconds
+    delay(2000);
 }
