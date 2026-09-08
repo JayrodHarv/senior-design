3aabeb19aef7d9f9aedb58031fd768890da8ee6a
Cole90-bit <colekimball919@gmail.com>
2026-09-08 14:44:53 -0500

    Changed buttons to interrupts.

--- Lab01_Temperature_Reader/embedded/src/main.cpp
+++ Lab01_Temperature_Reader/embedded/src/main.cpp
@@ -1,10 +1,3 @@
-//   //what for startup
-//   //ESP32 dev board espressif32
-//   //main loop
-//   //read DS18B20 serial data
-//   //send data to http server
-
-//   //cut power to esp32 dev board when switch is off.
 
 #include "network/network.h"
 #include "sensors/temperature.h"
@@ -14,19 +7,26 @@
 // Initialize the LCD with the appropriate pins
 LiquidCrystal lcd(14, 27, 26, 25, 33, 32);
 
-bool lastRawButton1State = HIGH; // Assume button is not pressed initially
-// bool lastButton2State = HIGH; // Assume button is not pressed initially
+volatile bool button1Interrupt = false; // Flag to indicate button 1 interrupt
+volatile bool button2Interrupt = false; // Flag to indicate button 2 interrupt
 
-bool stableButton1State = HIGH; // Stable state of button 1
+void IRAM_ATTR handleButton1Interrupt() {
+    button1Interrupt = true; // Set the flag when button 1 is pressed
+}
 
-unsigned long lastDebounceTime = 0; // The last time the button input was toggled
-const unsigned long debounceDelay = 50; // The debounce time; increase if the output flick
+void IRAM_ATTR handleButton2Interrupt() {
+    button2Interrupt = true; // Set the flag when button 2 is pressed
+}
+
 
 void setup() {
     pinMode(ButtonPin1, INPUT_PULLUP); // Set button pin 1 as input with pull-up resistor
     pinMode(ButtonPin2, INPUT_PULLUP); // Set button pin 2 as input
     // Start Serial Monitor
     Serial.begin(115200);
+
+    attachInterrupt(digitalPinToInterrupt(ButtonPin1), handleButton1Interrupt, FALLING); // Attach interrupt for button 1
+    attachInterrupt(digitalPinToInterrupt(ButtonPin2), handleButton2Interrupt, FALLING); // Attach interrupt for button 2
 
     // Connect to wifi
     // setupNetwork();
@@ -48,29 +48,35 @@ void setup() {
 void loop() {
 
     bool rawButton1State = digitalRead(ButtonPin1);
-    bool button2State = digitalRead(ButtonPin2);
+    bool rawButton2State = digitalRead(ButtonPin2);
     
-    if(rawButton1State != lastRawButton1State) {
-        lastDebounceTime = millis(); // Reset the debouncing timer
-    }
-    
-    if((millis() - lastDebounceTime) > debounceDelay) {
-        if(rawButton1State != stableButton1State) {
-            stableButton1State = rawButton1State;
+   if(button1Interrupt) {
+        button1Interrupt = false;
 
-            if(stableButton1State == LOW) {
-                temperatureSensor1Enabled = !temperatureSensor1Enabled; // Toggle the state of sensor 1
-                // Button 1 is pressed
-                Serial.println("Button 1 pressed");
-                if(temperatureSensor1Enabled) {
-                    Serial.println("Sensor 1 enabled");
-                } else {
-                    Serial.println("Sensor 1 disabled");
-                }
-            }
+        temperatureSensor1Enabled = !temperatureSensor1Enabled;
+
+        Serial.println("Button 1 pressed");
+
+        if(temperatureSensor1Enabled) {
+            Serial.println("Sensor 1 enabled");
+        } else {
+            Serial.println("Sensor 1 disabled");
         }
-    }   
-    lastRawButton1State = rawButton1State;
+    }
+
+    if(button2Interrupt) {
+        button2Interrupt = false;
+
+        temperatureSensor2Enabled = !temperatureSensor2Enabled;
+
+        Serial.println("Button 2 pressed");
+
+        if(temperatureSensor2Enabled) {
+            Serial.println("Sensor 2 enabled");
+        } else {
+            Serial.println("Sensor 2 disabled");
+        }
+    }
 
     if(temperatureSensor1Enabled) {
         //needs tied to the unique sensor id
@@ -91,36 +97,29 @@ void loop() {
         lcd.print("Sensor 1 Disabled     ");
     }
        
-    // if(button2State == HIGH && lastButton2State == LOW) {
-    //     temperatureSensor2Enabled = !temperatureSensor2Enabled; // Toggle the state of sensor 2
-    //     // Button 2 is pressed
-    //     Serial.println("Button 2 pressed");
-    //     if(temperatureSensor2Enabled) {
-    //         Serial.println("Sensor 2 enabled");
-    //     } else {
-    //         Serial.println("Sensor 2 disabled");
-    //     }
-    //     delay(50); // Debounce delay
-    // }
 
-    // lastButton2State = button2State;
+    if(temperatureSensor2Enabled) {
 
-    // if(temperatureSensor2Enabled){
-    //     //needs tied to the unique sensor id
-    //     //will be number 2
-    //     if(tempStatusCheck(sensor2, temperatureSensor2Enabled) == false){
-    //         //display to lcd that no device is connected
-    //         lcd.setCursor(0, 1);
-    //         lcd.print("No Device");           
-    //     } else {
-    //         float temperature = readTemperature(sensor2);
-    //         uploadTemperature(temperature);
-    //         lcd.setCursor(0, 1);
-    //         lcd.print("Temp: " + String(temperature) + " C");
-    //         //upload temp to database
-    //     }
-    // }
-   
+        if(tempStatusCheck(sensor2, temperatureSensor2Enabled) == false) {
+
+            lcd.setCursor(0, 1);
+            lcd.print("No Device       ");
+
+        } else {
+
+            float temperature = readTemperature(sensor2);
+            uploadTemperature(temperature);
+
+            lcd.setCursor(0, 1);
+            lcd.print("Temp: " + String(temperature) + " C       ");
+        }
+
+    } else {
+
+        lcd.setCursor(0, 1);
+        lcd.print("Sensor 2 Disabled");
+    }
+    
     unsigned long now = millis();
 
     // ----------------------------
